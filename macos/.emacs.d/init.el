6d779135c5b3ba0b21ece994281351802bbfda1a
LamT <lamtt77@gmail.com>
2020-10-13 21:53:55 +1100

    add MacOS emacs config

---
+++ macos/.emacs.d/init.el
@@ -0,0 +1,29 @@
+
+;; https://github.com/zamansky/dot-emacs/blob/master/init.el
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+ ;; (add-to-list 'package-archives
+ ;; 	     '("gnu" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-collection evil dired+ zenburn-theme which-key web-mode virtualenvwrapper use-package undo-tree try tabbar posframe pcre2el ox-reveal org-bullets noflet jedi iedit hydra hungry-delete htmlize ggtags flycheck expand-region emmet-mode elpy elfeed dumb-jump counsel-projectile beacon ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
