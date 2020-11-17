d85fc28f16853f09b0ff15fa27bbb38ef8e060e3
LamT <lam@lamhub.com>
2020-11-17 17:13:24 +1100

    tmux base index start from 1 instead of 0

--- common-home/.emacs.d/init.el
+++ common-home/.emacs.d/init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "CYRE" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Inconsolata" :foundry "CYRE" :slant normal :weight normal :height 110 :width normal)))))
 
 ;; '(package-selected-packages
 ;;   '(org-roam gnuplot auctex ox-report evil)))
